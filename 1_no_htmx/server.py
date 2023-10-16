074534c80b817f4db407d4fda45be404908a36a0
BiteCode! <contact@bitecode.dev>
2023-10-16 20:32:18 +0200

    Raw HTTP version

---
+++ 1_no_htmx/server.py
@@ -0,0 +1,48 @@
+import sys
+from pathlib import Path
+
+### BOILERPLATE TO MINIMIZE DEMO SETUP FOR YOU, IGNORE ###
+CUR_DIR = Path(__file__).resolve().parent
+sys.path.append(str(CUR_DIR.parent))
+
+import todo
+from bottle import TEMPLATE_PATH, debug, redirect, request, route, run, template
+
+TEMPLATE_PATH.append(str(CUR_DIR))
+### END BOILER PLATE ###
+
+
+@route("/")
+def index():
+    tasks = todo.list_todos()
+    counts = todo.count_todos()
+    return template("index", tasks=tasks, counts=counts)
+
+
+@route("/add", method="POST")
+def add():
+    title = request.forms.get("title")
+    if title:
+        todo.add_todo(title)
+    redirect("/")
+
+
+@route("/set_task_status", method="POST")
+def set_task_status():
+    all_tasks = todo.list_todos()
+    done_task_ids = set(int(task_id) for task_id in request.forms.getall("task"))
+    for task in all_tasks:
+        new_status = task["id"] in done_task_ids
+        todo.set_task_status(task["id"], new_status)
+    redirect("/")
+
+
+@route("/delete/<task_id:int>", method="GET")
+def delete(task_id):
+    todo.delete_todo(task_id)
+    redirect("/")
+
+
+if __name__ == "__main__":
+    debug(True)
+    run(host="localhost", port=8080, reloader=True)
